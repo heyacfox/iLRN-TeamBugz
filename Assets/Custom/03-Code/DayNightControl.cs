f4b86a113dd1b45b627a1396a5fb4cea512120d4
thexammer <thexammer@gmail.com>
2021-05-12 11:59:57 -0400

    Make DayNight pauseable

--- Assets/Custom/03-Code/DayNightControl.cs
+++ Assets/Custom/03-Code/DayNightControl.cs
@@ -30,6 +30,12 @@ public class DayNightControl : MonoBehaviour
     public Color SunsetColor;
     public Color NightColor;
 
+    private Coroutine runningCoroutine = null;
+
+    private float currentTime = 0f;
+
+    private List<LTDescr> tweens = new List<LTDescr>();
+
     private void Start()
     {
         // Set everything to night values
@@ -38,49 +44,104 @@ public class DayNightControl : MonoBehaviour
         DirectionalLight.intensity = 0;
         LeanTween.alpha(StarDome, 1f, 0.01f);
         LeanTween.alpha(NightDome, 1f, 0.01f);
-        StartCoroutine(DayCycle());
+        currentTime = 0f;
+        ProceedFrom(currentTime);
     }
 
-    private IEnumerator DayCycle()
+    public void Pause()
     {
-        float sunriseTime = DayLength * SunrisePercent;
-        float sunsetTime = DayLength * SunsetPercent;
-        float dayTime = DayLength - sunriseTime - sunsetTime;
-        // Sunrise
-        LeanTween.rotateAround(DirectionalLight.gameObject, DirectionalLight.transform.right, 60f, sunriseTime);
-        LeanTween.value(gameObject, (float val) => DirectionalLight.intensity = val, 0f, 1f, sunriseTime);
-        LeanTween.value(gameObject, (Color val) => DirectionalLight.color = val, NightColor, SunriseColor, sunriseTime);
-        LeanTween.alpha(StarDome, 0f, sunriseTime);
-        LeanTween.alpha(NightDome, 0f, sunriseTime);
-        yield return new WaitForSeconds(sunriseTime);
-
-        // Daytime
-        LeanTween.rotateAround(DirectionalLight.gameObject, DirectionalLight.transform.right, 180f, dayTime);
-        LeanTween.value(gameObject, (Color val) => DirectionalLight.color = val, SunriseColor, MiddayColor, dayTime / 2);
-        yield return new WaitForSeconds(dayTime / 2);
-        LeanTween.value(gameObject, (Color val) => DirectionalLight.color = val, MiddayColor, SunsetColor, dayTime / 2);
-        yield return new WaitForSeconds(dayTime / 2);
-
-        // Sunset
-        LeanTween.rotateAround(DirectionalLight.gameObject, DirectionalLight.transform.right, 60f, sunsetTime);
-        LeanTween.value(gameObject, (float val) => DirectionalLight.intensity = val, 1f, 0f, sunsetTime);
-        LeanTween.value(gameObject, (Color val) => DirectionalLight.color = val, SunsetColor, NightColor, sunsetTime);
-        LeanTween.alpha(StarDome, 1f, sunsetTime);
-        LeanTween.alpha(NightDome, 1f, sunsetTime);
-        yield return new WaitForSeconds(sunsetTime);
-
-        StartCoroutine(NightCycle());
+        tweens.ForEach((LTDescr tween) => { if (LeanTween.isTweening(tween.id)) { tween.pause(); } });
     }
 
-    private IEnumerator NightCycle()
+    /// <summary>
+    /// Resumes whatever tweens were paused previously.
+    /// </summary>
+    public void Resume()
     {
+        tweens.ForEach((LTDescr tween) => { if (!LeanTween.isTweening(tween.id)) { tween.resume(); } });
+    }
+
+    public void ProceedFrom(float time)
+    {
+        Pause();
+        tweens.Clear();
+        currentTime = time % (DayLength + NightLength);
+
+        float sunriseEnd = DayLength * SunrisePercent;
+        float dayEnd = DayLength - (DayLength * SunsetPercent);
+        if (currentTime < sunriseEnd)
+        {
+            Sunrise();
+        }
+        else if (currentTime < DayLength / 2)
+        {
+            StartDay();
+        }
+        else if (currentTime < dayEnd)
+        {
+            EndDay();
+        }
+        else if (currentTime < DayLength)
+        {
+            Sunset();
+        }
+        else
+        {
+            Night();
+        }
+    }
+
+    private void Sunrise()
+    {
+        float initTime = currentTime;
+        float time = (DayLength * SunrisePercent) - initTime;
+        tweens.Add(LeanTween.rotateAround(DirectionalLight.gameObject, DirectionalLight.transform.right, 60f, time));
+        tweens.Add(LeanTween.value(gameObject, (float val) => DirectionalLight.intensity = val, 0f, 1f, time));
+        tweens.Add(LeanTween.value(gameObject, (Color val) => DirectionalLight.color = val, NightColor, SunriseColor, time));
+        tweens.Add(LeanTween.alpha(StarDome, 0f, time));
+        tweens.Add(LeanTween.alpha(NightDome, 0f, time));
+        tweens.Add(LeanTween.value(gameObject, (float val) => currentTime = val, initTime, DayLength * SunrisePercent, time).setOnComplete(() => ProceedFrom(currentTime)));
+    }
+
+    private void StartDay()
+    {
+        float initTime = currentTime;
+        float time = (DayLength / 2) - initTime;
+        tweens.Add(LeanTween.rotateAround(DirectionalLight.gameObject, DirectionalLight.transform.right, 90f, time));
+        tweens.Add(LeanTween.value(gameObject, (Color val) => DirectionalLight.color = val, SunriseColor, MiddayColor, time));
+        tweens.Add(LeanTween.value(gameObject, (float val) => currentTime = val, initTime, DayLength / 2, time).setOnComplete(() => ProceedFrom(currentTime)));
+    }
+
+    private void EndDay()
+    {
+        float initTime = currentTime;
+        float time = (DayLength - (DayLength * SunsetPercent)) - initTime;
+        tweens.Add(LeanTween.rotateAround(DirectionalLight.gameObject, DirectionalLight.transform.right, 90f, time));
+        tweens.Add(LeanTween.value(gameObject, (Color val) => DirectionalLight.color = val, MiddayColor, SunsetColor, time));
+        tweens.Add(LeanTween.value(gameObject, (float val) => currentTime = val, initTime, DayLength - (DayLength * SunsetPercent), time).setOnComplete(() => ProceedFrom(currentTime)));
+    }
+
+    private void Sunset()
+    {
+        float initTime = currentTime;
+        float time = DayLength - initTime;
+        tweens.Add(LeanTween.rotateAround(DirectionalLight.gameObject, DirectionalLight.transform.right, 60f, time));
+        tweens.Add(LeanTween.value(gameObject, (float val) => DirectionalLight.intensity = val, 1f, 0f, time));
+        tweens.Add(LeanTween.value(gameObject, (Color val) => DirectionalLight.color = val, SunsetColor, NightColor, time));
+        tweens.Add(LeanTween.alpha(StarDome, 1f, time));
+        tweens.Add(LeanTween.alpha(NightDome, 1f, time));
+        tweens.Add(LeanTween.value(gameObject, (float val) => currentTime = val, initTime, DayLength, time).setOnComplete(() => ProceedFrom(currentTime)));
+    }
+
+    private void Night()
+    {
+        float initTime = currentTime;
+        float time = (DayLength + NightLength) - initTime;
         DirectionalLight.color = NightColor;
         DirectionalLight.transform.eulerAngles = new Vector3(-60f, -30f, 0f);
         DirectionalLight.intensity = 0;
         LeanTween.alpha(StarDome, 1f, 0.01f);
         LeanTween.alpha(NightDome, 1f, 0.01f);
-        yield return new WaitForSeconds(NightLength);
-
-        StartCoroutine(DayCycle());
+        tweens.Add(LeanTween.value(gameObject, (float val) => currentTime = val, initTime, DayLength + NightLength, time).setOnComplete(() => ProceedFrom(currentTime)));
     }
 }
